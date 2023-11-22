ec4aeb659c0f1e33b7a82824dd23c0ce437eaebf
JnManh <jonathan.manhaesilva@gsuite.iff.edu.br>
2023-11-22 12:00:30 -0300

    a

--- IFF/index.js
+++ IFF/index.js
@@ -6,15 +6,19 @@ app.use(express.static('public'))
 app.get("/", (req, res) => {
     res.render('home')
 })
+
 app.get("/doacao", (req, res) => {
     res.render('doacao')
 })
+
 app.get("/contato", (req, res) => {
     res.render('contato')
 })
+
 app.get("/sobre", (req, res) => {
     res.render('sobre')
 })
+
 app.get("/seca", (req, res) => {
     res.render('not1')
 })
@@ -22,5 +26,16 @@ app.get("/seca", (req, res) => {
 app.get("/moradores", (req, res) => {
     res.render('not2')
 })
+
+app.post('/test', (req, res) => {
+    const data = {
+        nome: req.body.name,
+        sobrenome: req.body.sobrenome,
+        valor: req.body.valor
+    }
+    res.render('form', { data });
+});
+
+
 
 app.listen(8080)
